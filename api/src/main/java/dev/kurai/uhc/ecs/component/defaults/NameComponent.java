e4f53070d76636cd8f937abde19d260449312d1f
kurai <67955616+kuraiwtf@users.noreply.github.com>
2026-05-12 22:30:34 +0200

    refactor: update NameComponent to use Supplier for dynamic name resolution
    Signed-off-by: kurai <67955616+kuraiwtf@users.noreply.github.com>

--- api/src/main/java/dev/kurai/uhc/ecs/component/defaults/NameComponent.java
+++ api/src/main/java/dev/kurai/uhc/ecs/component/defaults/NameComponent.java
@@ -1,23 +1,24 @@
 package dev.kurai.uhc.ecs.component.defaults;
 
 import dev.kurai.uhc.ecs.component.Component;
+import java.util.function.Supplier;
 import org.jetbrains.annotations.NotNull;
 
 public final class NameComponent implements Component {
 
-  private String name;
+  private Supplier<String> name;
 
   public NameComponent() {}
 
-  public NameComponent(final @NotNull String name) {
+  public NameComponent(final @NotNull Supplier<String> name) {
     this.name = name;
   }
 
   public String getName() {
-    return this.name;
+    return this.name.get();
   }
 
-  public void setName(final String name) {
+  public void setName(final Supplier<String> name) {
     this.name = name;
   }
 }
