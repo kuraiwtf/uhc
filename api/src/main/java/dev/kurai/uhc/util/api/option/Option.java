053266d17c10349c68f55d119bfcfb63fbe798cd
kurai <67955616+kuraiwtf@users.noreply.github.com>
2026-05-19 09:10:42 +0200

    feat: introduce RangedOption with support for value clamping and additional factory methods in Option interface
    Signed-off-by: kurai <67955616+kuraiwtf@users.noreply.github.com>

--- api/src/main/java/dev/kurai/uhc/util/api/option/Option.java
+++ api/src/main/java/dev/kurai/uhc/util/api/option/Option.java
@@ -5,11 +5,22 @@ import net.kyori.adventure.key.Keyed;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
-public sealed interface Option<T> extends Keyed permits OptionImpl {
+public sealed interface Option<T> extends Keyed permits OptionImpl, RangedOption {
 
   @Contract(value = "_, _ -> new", pure = true)
   static <T> @NotNull Option<T> option(final @NotNull Key key, final T value) {
     return new OptionImpl<>(key, value);
+  }
+
+  @Contract(value = "_, _ -> new", pure = true)
+  static @NotNull Option<Integer> positiveNumberOption(final Key key, final int defaultValue) {
+    return rangedOption(key, 0, Integer.MAX_VALUE, defaultValue);
+  }
+
+  @Contract(value = "_, _, _, _ -> new", pure = true)
+  static @NotNull Option<Integer> rangedOption(
+      final Key key, final int min, final int max, final int defaultValue) {
+    return new RangedOption(key, min, max, defaultValue);
   }
 
   @Override
---
+++ api/src/main/java/dev/kurai/uhc/util/api/option/RangedOption.java
@@ -0,0 +1,38 @@
+package dev.kurai.uhc.util.api.option;
+
+import net.kyori.adventure.key.Key;
+import org.jetbrains.annotations.NotNull;
+
+final class RangedOption implements Option<Integer> {
+
+  private final Key key;
+
+  private final int min;
+  private final int max;
+
+  private int current;
+
+  RangedOption(final Key key, final int min, final int max, final int defaultValue) {
+    this.key = key;
+
+    this.min = min;
+    this.max = max;
+
+    this.current = defaultValue;
+  }
+
+  @Override
+  public @NotNull Key key() {
+    return this.key;
+  }
+
+  @Override
+  public Integer getValue() {
+    return this.current;
+  }
+
+  @Override
+  public void setValue(final Integer value) {
+    this.current = Math.clamp(value, this.min, this.max);
+  }
+}
