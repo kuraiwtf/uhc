3630f2d70b0071751adff500e639eeaee0a5d189
kurai <67955616+kuraiwtf@users.noreply.github.com>
2026-05-20 22:53:36 +0200

    feat: extend TeamModule interface with team-related methods and implement in BuiltinModule
    Signed-off-by: kurai <67955616+kuraiwtf@users.noreply.github.com>

--- api/src/main/java/dev/kurai/uhc/module/team/module/TeamModule.java
+++ api/src/main/java/dev/kurai/uhc/module/team/module/TeamModule.java
@@ -1,3 +1,24 @@
 package dev.kurai.uhc.module.team.module;
 
-public interface TeamModule {}
+public interface TeamModule {
+
+  boolean teamView();
+
+  void teamView(final boolean teamView);
+
+  int teamSize();
+
+  void teamSize(final int teamSize);
+
+  default boolean ffa() {
+    return this.teamSize() == 1;
+  }
+
+  boolean friendlyFire();
+
+  void friendlyFire(final boolean friendlyFire);
+
+  boolean randomTeam();
+
+  void randomTeam(final boolean randomTeam);
+}
--- engine/src/main/java/dev/kurai/uhc/module/BuiltinModule.java
+++ engine/src/main/java/dev/kurai/uhc/module/BuiltinModule.java
@@ -2,15 +2,26 @@ package dev.kurai.uhc.module;
 
 import dev.kurai.uhc.UltraHardcoreAPI;
 import dev.kurai.uhc.module.team.module.TeamModule;
+import lombok.Getter;
+import lombok.Setter;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
+@Getter
+@Setter
 public final class BuiltinModule extends AbstractModule implements TeamModule {
+
+  private int teamSize;
+
+  private boolean teamView;
+  private boolean friendlyFire;
+  private boolean randomTeam;
 
   public BuiltinModule(final @NotNull UltraHardcoreAPI ultraHardcore) {
     super("uhc", "UHC", null, ultraHardcore);
+    this.teamSize = 1;
   }
 
   @Override
