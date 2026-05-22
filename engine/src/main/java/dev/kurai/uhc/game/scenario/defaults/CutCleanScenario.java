27a0d9b6f10ed4141bec8d2bb8afe0abd59df721
kurai <67955616+kuraiwtf@users.noreply.github.com>
2026-05-22 18:23:29 +0200

    refactor: simplify CutCleanScenario logic by replacing recipe map with predefined material/entity sets
    Signed-off-by: kurai <67955616+kuraiwtf@users.noreply.github.com>

--- engine/src/main/java/dev/kurai/uhc/game/scenario/defaults/CutCleanScenario.java
+++ engine/src/main/java/dev/kurai/uhc/game/scenario/defaults/CutCleanScenario.java
@@ -1,40 +1,39 @@
 package dev.kurai.uhc.game.scenario.defaults;
 
-import com.google.common.collect.Maps;
+import static org.bukkit.Material.*;
+import static org.bukkit.Material.DIAMOND;
+import static org.bukkit.Material.DIAMOND_ORE;
+import static org.bukkit.Material.GOLD_INGOT;
+
 import dev.kurai.uhc.UltraHardcoreAPI;
-import dev.kurai.uhc.event.defaults.scenario.CutCleanDropEvent;
 import dev.kurai.uhc.game.scenario.AbstractScenario;
+import java.util.Collection;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
 import net.minecraft.server.v1_8_R3.*;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.ExperienceOrb;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.inventory.FurnaceRecipe;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
+import org.jspecify.annotations.NonNull;
 
 public final class CutCleanScenario extends AbstractScenario implements Listener {
 
-  private static final Map<ItemStack, ItemStack> FURNACE_RECIPES = Maps.newHashMap();
+  private static final Map<Material, Material> ALLOWED_MATERIALS =
+      Map.of(
+          IRON_ORE, IRON_INGOT,
+          GOLD_ORE, GOLD_INGOT,
+          DIAMOND_ORE, DIAMOND);
 
-  static {
-    final var recipeIterator = Bukkit.getServer().recipeIterator();
-    while (recipeIterator.hasNext()) {
-      final var recipe = recipeIterator.next();
-
-      if (recipe instanceof final FurnaceRecipe furnaceRecipe) {
-        final var input = furnaceRecipe.getInput();
-        final var result = furnaceRecipe.getResult();
-
-        FURNACE_RECIPES.put(input.clone(), result.clone());
-      }
-    }
-  }
+  private static final Collection<EntityType> ALLOWED_ENTITIES =
+      Set.of(
+          EntityType.COW, EntityType.PIG, EntityType.CHICKEN, EntityType.SHEEP, EntityType.RABBIT);
 
   public CutCleanScenario(final @NotNull UltraHardcoreAPI ultraHardcore) {
     super("cut_clean", "Cut Clean", ultraHardcore);
@@ -45,91 +44,68 @@ public final class CutCleanScenario extends AbstractScenario implements Listener
     return new ItemStack(Material.IRON_INGOT);
   }
 
-  @EventHandler
-  public void onBlockBreak(final BlockBreakEvent event) {
+  @EventHandler(priority = EventPriority.LOWEST)
+  public void onBreak(final @NonNull BlockBreakEvent event) {
+    if (event.isCancelled()) {
+      return;
+    }
+
     final var block = event.getBlock();
-    final var type = block.getType();
-    if (!type.name().contains("ORE")) {
+    final var blockType = block.getType();
+    if (!ALLOWED_MATERIALS.containsKey(blockType)) {
       return;
     }
 
-    final var result = this.findFurnaceResult(new ItemStack(block.getType(), 1, block.getData()));
-    if (result == null) {
-      return;
+    final var player = event.getPlayer();
+    final var smeltedMaterial = ALLOWED_MATERIALS.get(blockType);
+
+    final var drops = block.getDrops();
+
+    block.setType(AIR);
+    player.giveExp(ThreadLocalRandom.current().nextInt(2, 6));
+
+    for (final var stack : drops) {
+      stack.setType(smeltedMaterial);
+      final var map = player.getInventory().addItem(stack);
+      if (map.isEmpty()) {
+        continue;
+      }
+      block.getWorld().dropItemNaturally(player.getLocation(), stack);
     }
-
-    if (type == Material.LAPIS_ORE) {
-      result.setAmount(ThreadLocalRandom.current().nextInt(4, 7));
-    }
-
-    final var hand = event.getPlayer().getItemInHand();
-    if (hand != null
-        && hand.hasItemMeta()
-        && hand.getItemMeta().hasEnchant(Enchantment.LOOT_BONUS_BLOCKS)) {
-      result.setAmount(
-          result.getAmount()
-              + ThreadLocalRandom.current()
-                  .nextInt(
-                      1, hand.getItemMeta().getEnchantLevel(Enchantment.LOOT_BONUS_BLOCKS) + 2));
-    }
-
-    final var experience =
-        this.calculateExperience(
-            result.getAmount(), ThreadLocalRandom.current().nextFloat(2f, 3.5f));
-    final var dropEvent =
-        this.ultraHardcore
-            .eventService()
-            .dispatchEvent(new CutCleanDropEvent(event.getPlayer(), result, experience));
-
-    block.setType(Material.AIR);
-    if (dropEvent.isCancelled()) {
-      return;
-    }
-
-    final var world = block.getWorld();
-    final var location = block.getLocation().clone().add(0.5, 0.5, 0.5);
-    world.dropItem(location, result);
-    final var orb = world.spawn(location, ExperienceOrb.class);
-    orb.setExperience(experience);
   }
 
-  private ItemStack findFurnaceResult(final ItemStack input) {
-    for (final var entry : FURNACE_RECIPES.entrySet()) {
-      final ItemStack recipeInput = entry.getKey();
+  @EventHandler
+  public void onDeath(final @NonNull EntityDeathEvent event) {
+    final var entity = event.getEntity();
+    if (!ALLOWED_ENTITIES.contains(entity.getType())) {
+      return;
+    }
 
-      if (recipeInput.getType() == input.getType()
-          && recipeInput.getDurability() == input.getDurability()) {
-        return entry.getValue().clone();
+    event.getDrops().clear();
+
+    final var world = entity.getWorld();
+    final var location = entity.getLocation();
+
+    final var leather = new ItemStack(LEATHER, 2);
+
+    switch (entity.getType()) {
+      case COW -> {
+        world.dropItemNaturally(location, new ItemStack(COOKED_BEEF, 2));
+        world.dropItemNaturally(location, leather);
+      }
+      case CHICKEN -> {
+        world.dropItemNaturally(location, new ItemStack(COOKED_CHICKEN, 2));
+        world.dropItemNaturally(location, new ItemStack(FEATHER, 2));
+      }
+      case PIG -> world.dropItemNaturally(location, new ItemStack(GRILLED_PORK, 2));
+      case SHEEP -> {
+        world.dropItemNaturally(location, new ItemStack(COOKED_MUTTON, 2));
+        world.dropItemNaturally(location, new ItemStack(WOOL));
+      }
+      case RABBIT -> {
+        world.dropItemNaturally(location, new ItemStack(COOKED_RABBIT, 2));
+        world.dropItemNaturally(location, leather);
       }
     }
-
-    for (final var entry : FURNACE_RECIPES.entrySet()) {
-      if (entry.getKey().getType() == input.getType()) {
-        return entry.getValue().clone();
-      }
-    }
-    return null;
-  }
-
-  private int calculateExperience(final int count, final float xpPerItem) {
-    if (xpPerItem == 0.0f) {
-      return 0;
-    }
-
-    final int totalXp;
-
-    if (xpPerItem < 1.0f) {
-      int j = (int) Math.floor((float) count * xpPerItem);
-      final float exactXp = (float) count * xpPerItem;
-      if (j < Math.ceil(exactXp) && Math.random() < (double) (exactXp - (float) j)) {
-        ++j;
-      }
-
-      totalXp = j;
-    } else {
-      totalXp = (int) ((float) count * xpPerItem);
-    }
-
-    return totalXp;
   }
 }
