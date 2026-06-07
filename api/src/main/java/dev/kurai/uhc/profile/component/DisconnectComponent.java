5fc5de16e833779ca926574ebfa3f5da772e4ce6
kurai <67955616+kuraiwtf@users.noreply.github.com>
2026-06-07 11:37:56 +0200

    refactor: remove unused fields from DisconnectComponent for streamlined player state handling
    Signed-off-by: kurai <67955616+kuraiwtf@users.noreply.github.com>

--- api/src/main/java/dev/kurai/uhc/profile/component/DisconnectComponent.java
+++ api/src/main/java/dev/kurai/uhc/profile/component/DisconnectComponent.java
@@ -4,9 +4,6 @@ import dev.kurai.uhc.ecs.component.Component;
 import java.time.Instant;
 import lombok.Getter;
 import lombok.Setter;
-import org.bukkit.Location;
-import org.bukkit.inventory.ItemStack;
-import org.jspecify.annotations.Nullable;
 
 @Getter
 @Setter
@@ -14,10 +11,6 @@ public final class DisconnectComponent implements Component {
 
   private long timeLeft;
   private Instant lastLogin;
-
-  private @Nullable Location lastLocation;
-  private ItemStack[] inventory = new ItemStack[36];
-  private ItemStack[] armor = new ItemStack[4];
 
   public DisconnectComponent(final long timeLeft, final Instant lastLogin) {
     this.timeLeft = timeLeft;
