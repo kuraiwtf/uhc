404b4f198264cdd062b5a612b21629d0839b0ca8
kurai <67955616+kuraiwtf@users.noreply.github.com>
2026-07-03 13:16:51 +0200

    refactor: reformat `build.gradle.kts` for consistent indentation style
    Signed-off-by: kurai <67955616+kuraiwtf@users.noreply.github.com>

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,35 +1,35 @@
 plugins {
-    id("uhc.java-library-conventions")
-    id("uhc.maven-publishing-conventions")
+  id("uhc.java-library-conventions")
+  id("uhc.maven-publishing-conventions")
 }
 
 java {
-    toolchain { languageVersion = JavaLanguageVersion.of(25) }
+  toolchain { languageVersion = JavaLanguageVersion.of(25) }
 
-    withSourcesJar()
-    withJavadocJar()
+  withSourcesJar()
+  withJavadocJar()
 }
 
 dependencies {
-    api(platform(libs.adventure.bom))
-    api(libs.bundles.apollo)
-    api(libs.bundles.adventure)
-    api(libs.bundles.jackson)
-    api(libs.menu.api)
-    api(libs.entity.lib)
-    api(libs.actionbar.api)
-    api(libs.lombok)
-    annotationProcessor(libs.lombok)
-    compileOnly(libs.packetevents.spigot)
-    compileOnly(libs.spigot.api)
+  api(platform(libs.adventure.bom))
+  api(libs.bundles.apollo)
+  api(libs.bundles.adventure)
+  api(libs.bundles.jackson)
+  api(libs.menu.api)
+  api(libs.entity.lib)
+  api(libs.actionbar.api)
+  api(libs.lombok)
+  annotationProcessor(libs.lombok)
+  compileOnly(libs.packetevents.spigot)
+  compileOnly(libs.spigot.api)
 }
 
 publishing {
-    publications.create<MavenPublication>("maven") {
-        from(components["java"])
+  publications.create<MavenPublication>("maven") {
+    from(components["java"])
 
-        groupId = project.group.toString()
-        artifactId = "${rootProject.name}-${project.name}"
-        version = project.version.toString()
-    }
+    groupId = project.group.toString()
+    artifactId = "${rootProject.name}-${project.name}"
+    version = project.version.toString()
+  }
 }
