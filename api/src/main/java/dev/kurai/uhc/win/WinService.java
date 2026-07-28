948cc36a1f0104f6a5cc9b19312db51c14801c08
kurai <67955616+kuraiwtf@users.noreply.github.com>
2026-07-29 01:14:11 +0200

    feat: add `WinService` interface and `WinServiceImpl` implementation to manage win conditions and celebrations
    Signed-off-by: kurai <67955616+kuraiwtf@users.noreply.github.com>

---
+++ api/src/main/java/dev/kurai/uhc/win/WinService.java
@@ -0,0 +1,12 @@
+package dev.kurai.uhc.win;
+
+public interface WinService {
+
+  WinCelebration<?> winCelebration();
+
+  void installWinCelebration(final WinCelebration<?> winCelebration);
+
+  WinCondition winCondition();
+
+  void installWinCondition(final WinCondition winCondition);
+}
