3c08191537b873fda33c7789c1d7888406bc7184
kurai <67955616+kuraiwtf@users.noreply.github.com>
2026-08-31 07:33:23 +0200

    feat: add `SkinComponent` and `SkinModificationComponent` for profile skin management
    Signed-off-by: kurai <67955616+kuraiwtf@users.noreply.github.com>

---
+++ api/src/main/java/dev/kurai/uhc/profile/component/SkinComponent.java
@@ -0,0 +1,6 @@
+package dev.kurai.uhc.profile.component;
+
+import dev.kurai.uhc.ecs.component.Component;
+import dev.kurai.uhc.util.Skin;
+
+public record SkinComponent(Skin skin) implements Component {}
---
+++ api/src/main/java/dev/kurai/uhc/profile/component/SkinModificationComponent.java
@@ -0,0 +1,5 @@
+package dev.kurai.uhc.profile.component;
+
+import dev.kurai.uhc.ecs.component.Component;
+
+public record SkinModificationComponent() implements Component {}
