08112a762f70305ad97b5375e8e6d867b236a5a2
kurai <67955616+kuraiwtf@users.noreply.github.com>
2026-09-04 08:32:01 +0200

    feat: annotate `command` and `command.help` packages with `@NullMarked` for null-safety compliance
    Signed-off-by: kurai <67955616+kuraiwtf@users.noreply.github.com>

---
+++ engine/src/main/java/dev/kurai/uhc/command/help/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package dev.kurai.uhc.command.help;
+
+import org.jspecify.annotations.NullMarked;
---
+++ engine/src/main/java/dev/kurai/uhc/command/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package dev.kurai.uhc.command;
+
+import org.jspecify.annotations.NullMarked;
