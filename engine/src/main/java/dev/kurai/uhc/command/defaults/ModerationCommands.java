6df4e5196a44dbbc5fd9c50d38781001c155f5e4
kurai <67955616+kuraiwtf@users.noreply.github.com>
2026-09-11 08:35:40 +0200

    chore: move inv see command to ModerationCommands
    Signed-off-by: kurai <67955616+kuraiwtf@users.noreply.github.com>

--- engine/src/main/java/dev/kurai/uhc/command/defaults/ModerationCommands.java
+++ engine/src/main/java/dev/kurai/uhc/command/defaults/ModerationCommands.java
@@ -4,10 +4,14 @@ import dev.kurai.uhc.UltraHardcoreAPI;
 import dev.kurai.uhc.command.annotation.Command;
 import dev.kurai.uhc.command.annotation.CommandMeta;
 import dev.kurai.uhc.command.argument.annotation.Argument;
+import dev.kurai.uhc.game.host.HostService;
 import dev.kurai.uhc.menu.list.PlayerListMenu;
+import dev.kurai.uhc.menu.spectator.InventoryViewMenu;
 import dev.kurai.uhc.module.power.AbstractPower;
 import dev.kurai.uhc.module.power.restriction.defaults.CooldownPowerRestriction;
 import dev.kurai.uhc.profile.Profile;
+import dev.kurai.uhc.profile.ProfileService;
+import dev.kurai.uhc.profile.component.SpectatorComponent;
 import dev.kurai.uhc.util.CC;
 import org.bukkit.entity.Player;
 import org.jspecify.annotations.NullMarked;
@@ -19,6 +23,38 @@ public final class ModerationCommands {
 
   public ModerationCommands(final UltraHardcoreAPI ultraHardcore) {
     this.ultraHardcore = ultraHardcore;
+  }
+
+  @Command(
+      @CommandMeta(
+          name = "invsee",
+          description = "Voir l'inventaire d'un joueur",
+          permission = "uhc.command.invsee"))
+  public void inventorySee(final Player player, final @Argument(name = "joueur") Player target) {
+    final HostService hostService = this.ultraHardcore.gameService().hostService();
+    final ProfileService profileService = this.ultraHardcore.profileService();
+    final Profile profile = profileService.getOrCreateProfile(player);
+    if (!hostService.hasHostAccess(player) && !profile.hasComponent(SpectatorComponent.class)) {
+      player.sendMessage(CC.prefix(CC.MISSING_PERMISSION));
+      return;
+    }
+
+    new InventoryViewMenu(player, target, profileService.getOrCreateProfile(target)).open();
+  }
+
+  @Command(
+      @CommandMeta(
+          name = "topluck",
+          description = "Voir le classement des minerais",
+          permission = "uhc.command.topluck"))
+  public void topLuck(final Player player) {
+    final HostService hostService = this.ultraHardcore.gameService().hostService();
+    final ProfileService profileService = this.ultraHardcore.profileService();
+    final Profile profile = profileService.getOrCreateProfile(player);
+    if (!hostService.hasHostAccess(player) && !profile.hasComponent(SpectatorComponent.class)) {
+      player.sendMessage(CC.prefix(CC.MISSING_PERMISSION));
+      return;
+    }
   }
 
   @Command(@CommandMeta(name = "list", permission = "uhc.command.list"))
