5b2c55c7e4c235083d2cbb2abe8337c0f008109b
kurai <67955616+kuraiwtf@users.noreply.github.com>
2026-09-21 23:14:16 +0200

    feat: add PvPTimer event listener and auto-resurrection logic during PvP phase
    Signed-off-by: kurai <67955616+kuraiwtf@users.noreply.github.com>

--- engine/src/main/java/dev/kurai/uhc/timer/builtin/PvPTimer.java
+++ engine/src/main/java/dev/kurai/uhc/timer/builtin/PvPTimer.java
@@ -1,26 +1,48 @@
 package dev.kurai.uhc.timer.builtin;
 
+import dev.kurai.uhc.UltraHardcoreAPI;
+import dev.kurai.uhc.event.defaults.game.death.GamePreDeathEvent;
 import dev.kurai.uhc.timer.AbstractTimer;
 import dev.kurai.uhc.timer.annotation.Duration;
+import dev.kurai.uhc.util.CC;
 import dev.kurai.uhc.util.api.annotation.Identifier;
 import dev.kurai.uhc.util.api.annotation.Name;
-import dev.kurai.uhc.world.WorldService;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 
 @Identifier(PvPTimer.IDENTIFIER)
 @Name("PvP")
 @Duration(min = 15 * 60, defaultValue = 20 * 60, max = 60 * 60)
-public final class PvPTimer extends AbstractTimer {
+public final class PvPTimer extends AbstractTimer implements Listener {
 
   static final String IDENTIFIER = "pvp";
 
-  private final WorldService worldService;
+  private final UltraHardcoreAPI ultraHardcore;
 
-  public PvPTimer(final WorldService worldService) {
-    this.worldService = worldService;
+  public PvPTimer(final UltraHardcoreAPI ultraHardcore) {
+    this.ultraHardcore = ultraHardcore;
+  }
+
+  @Override
+  public void onStart() {
+    this.ultraHardcore.eventService().registerListener(this);
   }
 
   @Override
   public void onEnd() {
-    this.worldService.getWorld().setPVP(true);
+    this.ultraHardcore.eventService().unregisterListener(this);
+    this.ultraHardcore.worldService().getWorld().setPVP(true);
+  }
+
+  @EventHandler
+  public void onPreDeath(final GamePreDeathEvent event) {
+    final Player player = event.getVictim().getPlayer();
+    if (player != null) {
+      player.sendMessage(CC.prefix("Vous avez été automatiquement§d ressuscité§f."));
+      event.setRespawnLocation(player.getLocation());
+    }
+
+    event.setCancelled(true);
   }
 }
